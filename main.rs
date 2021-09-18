be9a5f7356d74e2e8348ed86d350931f321586ed
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-09-18 16:13:53 +0200

    Make a very simple hello, world! webserver

--- main.rs
+++ main.rs
@@ -1,3 +1,45 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:8000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let contents = "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"utf-8\">
+    <title>title</title>
+    <link rel=\"stylesheet\" href=\"style.css\">
+    <script src=\"script.js\"></script>
+  </head>
+  <body>
+    <strong>Hello, World!</strong>
+  </body>
+</html>";
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 1, 4);
+    }
 }
